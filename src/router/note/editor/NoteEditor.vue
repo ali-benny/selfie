76dbd29cda604778e28df36251288c76eac254f4
Alice Benatti <alice17bee@gmail.com>
2024-10-26 20:25:26 +0200

    feat: delete readers drag-n-drop

--- src/router/note/editor/NoteEditor.vue
+++ src/router/note/editor/NoteEditor.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="container mx-auto">
-    <div class="flex justify-between m-2 flex-wrap">
+  <div class="container mx-auto static">
+    <div
+      class="trash-bin z-10 absolute inset-x-2/4 bottom-20 w-16 h-16 !bg-error text-base-300 justify-center items-center rounded-full hidden shadow shadow-error"
+      @dragover.prevent
+      @drop="onDrop"
+    >
+      <Icon icon="fluent:delete-12-filled" class="text-3xl" />
+    </div>
+    <div class="flex justify-between flex-col md:flex-row *:w-full m-2 items-center">
       <div class="flex flex-row items-center">
         <input
           type="text"
-          class="input border !input-bordered input-primary text-2xl font-bold w-dvw md:w-auto"
+          class="input border !input-bordered input-primary text-2xl font-bold w-auto"
           v-model="title"
         />
         <!-- <button
@@ -16,21 +23,33 @@
         <Icon icon="fluent:checkmark-12-filled" v-else />
       </button> -->
       </div>
-
-      <div class="flex justify-end items-center">
-        <div class="avatar-group -space-x-4 hover:-space-x-0 rtl:space-x-reverse">
-          <div v-for="reader in this.readers" :key="reader._id" class="avatar h-10">
+      <div class="flex justify-between flex-wrap lg:justify-end items-center">
+        <div
+          class="avatar-group -space-x-4 hover:-space-x-0 transition hover:-translate-x-1 ease-in-out duration-300 rtl:space-x-reverse"
+        >
+          <div
+            v-for="reader in readers_verbose"
+            :key="reader._id"
+            class="relative avatar h-10 hover:cursor-pointer transition ease-in-out hover:scale-125 duration-300"
+            draggable="true"
+            @dragstart="onDragStart(reader)"
+            @dragend="onDragEnd"
+            @mouseover="showTrashBin"
+            @mouseleave="hideTrashBin"
+          >
             <img
-              class="mask mask-circle !bg-secondary"
+              class="mask mask-circle !bg-secondary hover:!bg-error"
               :src="reader.image"
               :title="reader.name + ' ' + reader.surname"
             />
           </div>
         </div>
-        <UserShare :content="id" type="Note"></UserShare>
-        <button class="btn text-xl btn-primary my-2 rounded-box" @click="saveNote">
-          <Icon icon="fluent:save-32-filled" /> Save
-        </button>
+        <div class="flex items-center">
+          <UserShare :content="id" type="Note"></UserShare>
+          <button class="btn text-xl btn-primary my-2 rounded-box" @click="saveNote">
+            <Icon icon="fluent:save-32-filled" /> Save
+          </button>
+        </div>
       </div>
     </div>
     <div>
@@ -59,7 +78,7 @@
 <script>
 import { useToast } from 'vue-toastification'
 import { initializeEditor, getEditNoteTitle, getEditNoteId } from './editor.js'
-import { getNoteTags, getReaders, saveNoteMongo, saveTodoMongo } from './note.js'
+import { getNoteTags, getReaders, getReadersIds, saveNoteMongo, saveTodoMongo } from './note.js'
 import { getTags, createTag } from '@/router/note/editor/tags'
 import UserShare from '@/components/UserShare.vue'
 import { useUserStore } from '@/stores/account'
@@ -72,7 +91,8 @@ export default {
     if (this.id != null) {
       this.title = getEditNoteTitle()
       this.selectedTags = await getNoteTags(this.id)
-      this.readers = await getReaders(this.id)
+      this.readers = await getReadersIds(this.id)
+      this.readers_verbose = await getReaders(this.id)
     }
   },
   directives: {
@@ -94,7 +114,9 @@ export default {
       share: false,
       id: '',
       userStore: useUserStore(),
-      readers: []
+      readers: [],
+      readers_verbose: [],
+      draggedReader: null
     }
   },
   methods: {
@@ -115,7 +137,8 @@ export default {
           filename: this.title,
           data: outputData,
           tags: this.selectedTags,
-          author: this.userStore.loggedUser._id
+          author: this.userStore.loggedUser._id,
+          readers: this.readers
         })
 
         // Check for checklist type and save to todo collection
@@ -141,20 +164,78 @@ export default {
     async addTag(event) {
       const newTag = event.target.value
       if (this.id == null) {
-        // needed a note id => save it
+        // needed a note id => savethe note before
         const outputData = await this.editor.save()
         this.id = await saveNoteMongo({
           id: this.id,
           filename: this.title,
           data: outputData,
           tags: this.selectedTags,
-          author: this.userStore.loggedUser._id
+          author: this.userStore.loggedUser._id,
+          readers: this.readers
         })
       }
       if (newTag && !this.tags.includes(newTag)) {
         createTag(this.id, newTag)
         this.tags.push(newTag)
         this.selectedTags.push(newTag)
+      }
+    },
+    async removeShare(reader) {
+      const toast = useToast()
+      const index = this.readers.indexOf(reader._id)
+      if (index !== -1) this.readers.splice(index, 1)
+
+      if (this.readers_verbose[reader._id]) {
+        delete this.readers_verbose[reader._id]
+      }
+
+      try {
+        await saveNoteMongo({
+          id: this.id,
+          readers: this.readers
+        })
+
+        toast.success(`Removed ${reader.name} ${reader.surname} from readers.`)
+      } catch (error) {
+        console.error('Failed to save updated readers:', error)
+        toast.error('Failed to remove reader')
+        // Ricarico i readers in caso di errore per mantenere la sincronizzazione
+        this.readers = await getReaders(this.id)
+        this.readers_verbose = await getReadersIds(this.readers)
+      }
+    },
+
+    onDragStart(reader) {
+      // Salvo il reader che sta per essere trascinato
+      this.draggedReader = reader
+      document.querySelector('.trash-bin').classList.remove('hidden')
+      document.querySelector('.trash-bin').classList.add('flex')
+    },
+
+    onDragEnd() {
+      // Nascondo il cestino
+      document.querySelector('.trash-bin').classList.add('hidden')
+      document.querySelector('.trash-bin').classList.remove('flex')
+    },
+
+    async onDrop() {
+      if (this.draggedReader && this.draggedReader._id) {
+        await this.removeShare(this.draggedReader)
+        this.draggedReader = null
+      }
+    },
+
+    showTrashBin() {
+      document.querySelector('.trash-bin').classList.remove('hidden')
+      document.querySelector('.trash-bin').classList.add('flex')
+    },
+
+    hideTrashBin() {
+      // Nascondo il cestino solo se non sto trascinando
+      if (!this.draggedReader) {
+        document.querySelector('.trash-bin').classList.add('hidden')
+        document.querySelector('.trash-bin').classList.remove('flex')
       }
     }
   },
