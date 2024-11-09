1bd462cc58444efb6ffd8a6fe522a7ebe3eb04a6
Alice Benatti <alice17bee@gmail.com>
2024-11-09 19:14:24 +0100

    feat: using swapy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="grid grid-cols-3 m-10 gap-5 place-content-center">
-    <div class="col-md-6 p-3 rounded-xl shadow-sm bg-base-200">Calendar</div>
-    <div class="col-span-2 p-3 rounded-xl shadow-sm bg-base-200">
-      <h4>Last Notes</h4>
-      <NoteView viewMode="list" :lastModified="3" :edit="false" :extended="false"></NoteView>
+  <div ref="container" class="container grid grid-cols-3 gap-5 place-content-center h-[85dvh]">
+    <div class="bg-base-200 rounded-xl" data-swapy-slot="1">
+      <div class="col-md-6 p-3 rounded-xl shadow-sm bg-surface-0" data-swapy-item="calendar">
+        Calendar
+      </div>
     </div>
-    <div class="h-100 p-3 bg-light rounded-xl shadow-sm bg-base-200">Pomodoro Timer</div>
-    <div class="col-span-2">
-      <div class="h-100 p-3 rounded-xl shadow-sm bg-base-200">Projects' Gantt View</div>
+    <div class="bg-base-200 col-span-2 rounded-xl" data-swapy-slot="2">
+      <div class="p-3 rounded-xl shadow-sm bg-surface-0" data-swapy-item="notes">
+        <h4>Last Notes</h4>
+        <NoteView viewMode="list" :lastModified="3" :edit="false" :extended="false"></NoteView>
+      </div>
+    </div>
+    <div class="bg-base-200 col-span-2 rounded-xl" data-swapy-slot="3">
+      <div class="h-100 min-h-36 p-3 rounded-xl shadow-sm bg-surface-0" data-swapy-item="project">
+        Projects' Gantt View
+      </div>
+    </div>
+    <div class="bg-base-200 rounded-xl" data-swapy-slot="4">
+      <div class="h-100 p-3 bg-light rounded-xl shadow-sm bg-surface-0" data-swapy-item="pomodoro">
+        Pomodoro Timer
+      </div>
     </div>
   </div>
   <!-- <RouterLink to="/login">Login</RouterLink> -->
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { createSwapy } from 'swapy'
 import NoteView from '@/components/NoteView.vue'
+
+const container = ref(null)
+onMounted(() => {
+  if (container.value) {
+    const swapy = createSwapy(container.value, {
+      animation: 'dynamic' // Può essere 'dynamic', 'spring' o 'none'
+    })
+    swapy.onSwap(({ data }) => {
+      localStorage.setItem('dashboard', JSON.stringify(data.object))
+    })
+  }
+})
 </script>
