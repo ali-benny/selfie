ac8054567e70113a9e08f26595983dd256b78a00
Alice Benatti <alice17bee@gmail.com>
2025-03-13 17:11:06 +0100

    chore: add border for selected group

--- src/components/group/GroupList.vue
+++ src/components/group/GroupList.vue
@@ -26,7 +26,7 @@ const selectGroup = (group) => {
 
 <template>
   <div class="flex flex-col gap-2 overflow-y-auto max-h-3/4">
-    <div v-for="group in groups" :key="group._id" class="collapse collapse-plus  prose" :class="group._id === selected ? 'bg-base-200' : 'bg-surface-0'">
+    <div v-for="group in groups" :key="group._id" class="collapse collapse-plus  prose" :class="group._id === selected ? 'bg-base-200 border-2  border-primary' : 'bg-surface-0'">
       <input type="checkbox" class="peer" />
       <div class="collapse-title flex items-center gap-2 font-bold">
         <Icon icon="mingcute:group-3-fill"></Icon>
