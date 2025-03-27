a70a6cbb7bb4a9e08e374cb7d3fa3cd4f75c2b48
Leonardo Verrone <verrone.leo@gmail.com>
2025-03-27 11:30:55 +0100

    fix: centered pomo animation in component, bug on config update

--- src/components/pomodoro/PomodoroAnimation.vue
+++ src/components/pomodoro/PomodoroAnimation.vue
@@ -4,7 +4,7 @@
     width="100%"
     height="100%"
     ref="animationElem"
-    class="overflow-hidden stroke-[.6em]"
+    class="overflow-visible stroke-[.3em] p-[0.15rem]"
     viewBox="0 0 100 100"
     preserveAspectRatio="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -21,27 +21,43 @@
         vector-effect="non-scaling-stroke"
         points="50,0 0,0 0,100, 100,100 100,0 50,0"
         class="linecap-round"
-        :stroke-dasharray="progressPathLength"
+        :stroke-dasharray="pathLength"
       ></polygon>
     </g>
   </svg>
   <svg
     v-else
-    class="overflow-visible stroke-surface stroke-[.3em]"
+    ref="animationElem"
+    class="overflow-visible stroke-surface stroke-[.5em] p-[.25rem]"
     viewBox="0 0 100 100"
     xmlns="http://www.w3.org/2000/svg"
   >
     <g class="fill-none linecap-round">
-      <line id="dot" x1="50" y1="0" x2="50" y2="0" :stroke="breakColor" pathLength="1" />
-      <circle cx="50" cy="50" r="50" class="stroke-surface-0/50" />
+      <line
+        id="dot"
+        x1="50"
+        y1="0"
+        x2="50"
+        y2="0"
+        vector-effect="non-scaling-stroke"
+        :stroke="breakColor"
+        pathLength="1"
+      />
+      <circle
+        cx="50"
+        cy="50"
+        r="50"
+        vector-effect="non-scaling-stroke"
+        class="stroke-surface-0/50"
+      />
       <circle
         ref="progress"
+        vector-effect="non-scaling-stroke"
         class="stroke-primary origin-center -rotate-90"
         cx="50"
         cy="50"
         r="50"
-        pathLength="1"
-        stroke-dasharray="1"
+        :stroke-dasharray="pathLength"
       />
     </g>
   </svg>
@@ -51,59 +67,13 @@
 import { flavors } from '@catppuccin/palette'
 import { usePomodoroStore } from '@/stores/pomodoro'
 import { storeToRefs } from 'pinia'
-import { computed, onMounted, toRaw, useTemplateRef, watch } from 'vue'
-import { reactiveComputed, toReactive, useElementSize, useMounted } from '@vueuse/core'
+import { computed, onMounted, useTemplateRef, watch } from 'vue'
+import { useElementSize, useMounted } from '@vueuse/core'
 
 const pomodoroColor = flavors.macchiato.colors.red.hex
 const breakColor = flavors.macchiato.colors.lavender.hex
 
 const isMounted = useMounted()
-/*
- * Actual animation object of WEB Animation API
- */
-let animation = null
-
-const pomodoroStore = usePomodoroStore()
-const pomodoro = toReactive(storeToRefs(pomodoroStore).pomodoro)
-const currentConfig = toReactive(storeToRefs(pomodoroStore).currentConfig)
-
-const animationElem = useTemplateRef('animationElem')
-const animationSize = toReactive(useElementSize(animationElem, { width: 0, height: 0 }))
-
-const progress = useTemplateRef('progress')
-
-/*
- * Used when animation is widget to make the animation responsive
- */
-const progressPathLength = computed(() => (animationSize.width + animationSize.height) * 2)
-
-const pomodoroKeyframes = reactiveComputed(() => {
-  return {
-    value: [
-      { stroke: pomodoroColor, strokeDashoffset: 0 },
-      { stroke: breakColor, strokeDashoffset: widget ? -progressPathLength.value : 1 }
-    ]
-  }
-})
-
-const breakKeyframes = reactiveComputed(() => {
-  return {
-    value: [
-      { stroke: breakColor, strokeDashoffset: widget ? progressPathLength.value : -1 },
-      { stroke: pomodoroColor, strokeDashoffset: 0 }
-    ]
-  }
-})
-
-const animationOptions = reactiveComputed(() => {
-  return {
-    duration: pomodoro.initialTimer * 1000,
-    fill: 'forwards',
-    easing: 'linear'
-  }
-})
-
-const currentTime = computed(() => pomodoro.initialTimer - pomodoro.timer)
 
 const { widget } = defineProps({
   widget: {
@@ -112,13 +82,71 @@ const { widget } = defineProps({
   }
 })
 
-onMounted(() => restart())
+/*
+ * Actual animation object of WEB Animation API
+ */
+let animation = null
 
-watch([() => currentConfig._id, () => pomodoro.phase], () => restart())
+const { pomodoro, currentConfig } = storeToRefs(usePomodoroStore())
+
+const animationElem = useTemplateRef('animationElem')
+const progress = useTemplateRef('progress')
+const { width, height } = useElementSize(animationElem, {
+  width: 0,
+  height: 0
+})
+
+/*
+ * Used when animation is widget to make the animation responsive
+ */
+const pathLength = computed(() => {
+  const pathLength = widget
+    ? (width.value + height.value) * 2
+    : (width.value - 2 * convertRemToPixels(0.25)) * Math.PI
+  return Math.round(pathLength)
+})
+
+// const pomodoroKeyframes = computed(() => [
+//   { stroke: pomodoroColor, strokeDashoffset: 0 },
+//   { stroke: breakColor, strokeDashoffset: -pathLength.value }
+// ])
+
+// const breakKeyframes = computed(() => [
+//   { stroke: breakColor, strokeDashoffset: pathLength.value },
+//   { stroke: pomodoroColor, strokeDashoffset: 0 }
+// ])
+
+const pomodoroKeyframes = computed(() => [
+  { stroke: pomodoroColor, strokeDashoffset: 0 },
+  { stroke: breakColor, strokeDashoffset: pathLength.value * (widget ? -1 : 1) }
+])
+
+const breakKeyframes = computed(() => [
+  { stroke: breakColor, strokeDashoffset: pathLength.value * (widget ? 1 : -1) },
+  { stroke: pomodoroColor, strokeDashoffset: 0 }
+])
+
+const animationOptions = computed(() => ({
+  duration: pomodoro.value.initialTimer * 1000,
+  fill: 'forwards',
+  easing: 'linear'
+}))
+
+const currentTime = computed(() => pomodoro.value.initialTimer - pomodoro.value.timer)
+
+onMounted(() => {
+  restart()
+})
+
+watch([width, height, currentConfig], () => restart())
+watch(
+  () => pomodoro.value.phase,
+  () => restart()
+)
 
 watch(currentTime, () => {
   if (!animation) return
-  if (pomodoro.running) {
+  if (pomodoro.value.running) {
     if (animation.playState !== 'running') animation.play()
   } else if (animation.playState !== 'paused') {
     animation.pause()
@@ -127,21 +155,14 @@ watch(currentTime, () => {
 })
 
 watch(
-  () => pomodoro.running,
+  () => pomodoro.value.running,
   () => {
     if (!animation) return
 
-    if (pomodoro.running) {
+    if (pomodoro.value.running) {
       animation.currentTime = currentTime.value * 1000
       animation.play()
     } else animation.pause()
-  }
-)
-
-watch(
-  () => animationSize.width,
-  () => {
-    if (widget) restart()
   }
 )
 
@@ -155,16 +176,19 @@ function restart() {
 
   const keyframes = new KeyframeEffect(
     progress.value,
-    toRaw(pomodoro.phase === 'pomodoro' ? pomodoroKeyframes.value : breakKeyframes.value),
-    animationOptions
+    pomodoro.value.phase === 'pomodoro' ? pomodoroKeyframes.value : breakKeyframes.value,
+    animationOptions.value
   )
 
   animation = new Animation(keyframes, document.timeline)
-
   animation.currentTime = currentTime.value * 1000
 
-  if (pomodoro.running) animation.play()
+  if (pomodoro.value.running) animation.play()
   else animation.pause()
+}
+
+function convertRemToPixels(rem) {
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
 }
 </script>
 
