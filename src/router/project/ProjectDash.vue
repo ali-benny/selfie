8e290ed723dcb0959c6a5615b215e5f8225426b8
Alice Benatti <alice17bee@gmail.com>
2025-04-05 12:42:51 +0200

    style: using catppuccin for vue-ganttastic

--- src/router/project/ProjectDash.vue
+++ src/router/project/ProjectDash.vue
@@ -1,10 +1,11 @@
 <template>
   <g-gantt-chart
     chart-start="2021-07-12 12:00"
-    chart-end="2021-07-14 12:00"
+    chart-end="2021-07-13 12:00"
     precision="hour"
     bar-start="myBeginDate"
     bar-end="myEndDate"
+	:color-scheme="catppuccin_schema"
   >
     <g-gantt-row label="My row 1" :bars="row1BarList" />
     <g-gantt-row label="My row 2" :bars="row2BarList" />
@@ -13,6 +14,21 @@
 
 <script setup>
 import { ref } from 'vue'
+import { flavors } from '@catppuccin/palette'
+
+const themeColors = flavors.macchiato.colors
+
+const catppuccin_schema = {
+	primary: themeColors.surface0.hex,
+	secondary: themeColors.surface2.hex,
+	ternary: themeColors.base.hex,
+	quartenary: themeColors.mantle.hex,
+	hoverHighlight: themeColors.pink.hex,
+	markerCurrentTime: themeColors.lavender.hex,
+	text: themeColors.text.hex,
+	background: themeColors.crust.hex,
+	toast: themeColors.red.hex
+}
 
 const row1BarList = ref([
   {
@@ -32,13 +48,7 @@ const row2BarList = ref([
     ganttBarConfig: {
       id: 'another-unique-id-2',
       hasHandles: true,
-      label: 'Hey, look at me',
-      style: {
-        // arbitrary CSS styling for your bar
-        background: '#e09b69',
-        borderRadius: '20px',
-        color: 'black'
-      }
+      label: 'Hey, look at me'
     }
   }
 ])
