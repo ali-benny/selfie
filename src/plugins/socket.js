2fbd2953e179755db9dbcee12b9caa51a3c86f8b
Leonardo Verrone <verrone.leo@gmail.com>
2025-06-24 16:17:14 +0200

    fix: socket SERVER_URL

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,15 +1,12 @@
 import { io } from 'socket.io-client'
 import { SERVER_URL } from '@/const.js'
 
-const socket = io(
-  process.env.NODE_ENV === 'production' ? 'https://your-deployed-backend-url.com' : SERVER_URL,
-  {
-    reconnectionAttempts: 5,
-    reconnectionDelay: 1000,
-    autoConnect: true,
-    timeout: 10000,
-    transports: ['websocket', 'polling']
-  }
-)
+const socket = io(SERVER_URL, {
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  autoConnect: true,
+  timeout: 10000,
+  transports: ['websocket', 'polling']
+})
 
 export default socket
